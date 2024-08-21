83e26d098c608620d6f73472fad63b9aae505a4c
Shota Ide <ide.shota.dev@gmail.com>
2024-08-22 00:28:29 +0900

    fix: replace web socket server

--- app/src/main/java/ide/shota/colomney/websockettest/MainActivity.kt
+++ app/src/main/java/ide/shota/colomney/websockettest/MainActivity.kt
@@ -38,7 +38,7 @@ class WebSocketClient : WebSocketListener() {
             )
             .build()
         val request = Request.Builder()
-            .url("wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
+            .url("wss://echo.websocket.org")
             .build()
         webSocket = client.newWebSocket(request, this)
     }
